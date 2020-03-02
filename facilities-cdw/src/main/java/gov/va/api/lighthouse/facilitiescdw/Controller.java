4995104d334fe2ee54443e4d0ca4a603c5b1b669
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-03-02 10:41:00 -0500

    Restore database access

--- facilities-cdw/src/main/java/gov/va/api/lighthouse/facilitiescdw/Controller.java
+++ facilities-cdw/src/main/java/gov/va/api/lighthouse/facilitiescdw/Controller.java
@@ -1,9 +1,17 @@
 package gov.va.api.lighthouse.facilitiescdw;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = {"/", "/api"})
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class Controller {
-  // private final JdbcTemplate jdbc;
+  private final JdbcTemplate jdbc;
 
-  //  private static <T> T checkNotNull(T maybe) {
-  //    if (maybe == null) {
-  //      throw new IllegalStateException("Expected non-null value");
-  //    }
-  //    return maybe;
-  //  }
+  private static <T> T checkNotNull(T maybe) {
+    if (maybe == null) {
+      throw new IllegalStateException("Expected non-null value");
+    }
+    return maybe;
+  }
 
   @RequestMapping(
       value = "/mental-health-contact",
@@ -35,55 +43,52 @@ public class Controller {
 
   @SneakyThrows
   private String mentalHealthContactInfo() {
-    //    List<String> results = new ArrayList<>();
-    //    try (Connection connection = checkNotNull(jdbc.getDataSource()).getConnection()) {
-    //      try (PreparedStatement statement =
-    //          connection.prepareStatement("SELECT * FROM App.VHA_Mental_Health_Contact_Info")) {
-    //        // statement.setString(1, icn);
-    //        try (ResultSet resultSet = statement.executeQuery()) {
-    //          ResultSetMetaData rsmd = resultSet.getMetaData();
-    //          int columnCount = rsmd.getColumnCount();
-    //          while (resultSet.next()) {
-    //            List<String> values = new ArrayList<>(columnCount);
-    //            for (int i = 1; i <= columnCount; i++) {
-    //              values.add(rsmd.getColumnName(i) + ":" + resultSet.getString(i));
-    //            }
-    //            results.add(values.stream().collect(Collectors.joining(" ")));
-    //          }
-    //        }
-    //      }
-    //      return "SELECT * FROM App.VHA_Mental_Health_Contact_Info\n"
-    //          + results.stream().collect(Collectors.joining("\n"));
-    //    }
-    return "mental health";
+    List<String> results = new ArrayList<>();
+    try (Connection connection = checkNotNull(jdbc.getDataSource()).getConnection()) {
+      try (PreparedStatement statement =
+          connection.prepareStatement("SELECT * FROM App.VHA_Mental_Health_Contact_Info")) {
+        // statement.setString(1, icn);
+        try (ResultSet resultSet = statement.executeQuery()) {
+          ResultSetMetaData rsmd = resultSet.getMetaData();
+          int columnCount = rsmd.getColumnCount();
+          while (resultSet.next()) {
+            List<String> values = new ArrayList<>(columnCount);
+            for (int i = 1; i <= columnCount; i++) {
+              values.add(rsmd.getColumnName(i) + ":" + resultSet.getString(i));
+            }
+            results.add(values.stream().collect(Collectors.joining(" ")));
+          }
+        }
+      }
+      return "SELECT * FROM App.VHA_Mental_Health_Contact_Info\n"
+          + results.stream().collect(Collectors.joining("\n"));
+    }
   }
 
   /** Query stop code wait times. */
   @SneakyThrows
   public String stopCodeWaitTimes() {
-    //    List<String> results = new ArrayList<>();
-    //    try (Connection connection = checkNotNull(jdbc.getDataSource()).getConnection()) {
-    //      try (PreparedStatement statement =
-    //          connection.prepareStatement(
-    //              "SELECT * FROM App.VHA_Stop_Code_Wait_Times_Paginated(1, 100)")) {
-    //        // statement.setString(1, icn);
-    //        try (ResultSet resultSet = statement.executeQuery()) {
-    //          ResultSetMetaData rsmd = resultSet.getMetaData();
-    //          int columnCount = rsmd.getColumnCount();
-    //          while (resultSet.next()) {
-    //            List<String> values = new ArrayList<>(columnCount);
-    //            for (int i = 1; i <= columnCount; i++) {
-    //              values.add(rsmd.getColumnName(i) + ":" + resultSet.getString(i));
-    //            }
-    //            results.add(values.stream().collect(Collectors.joining(" ")));
-    //          }
-    //        }
-    //      }
-    //      return "SELECT * FROM App.VHA_Stop_Code_Wait_Times_Paginated(1, 100)\n"
-    //          + results.stream().collect(Collectors.joining("\n"));
-    //    }
-
-    return "stop codes";
+    List<String> results = new ArrayList<>();
+    try (Connection connection = checkNotNull(jdbc.getDataSource()).getConnection()) {
+      try (PreparedStatement statement =
+          connection.prepareStatement(
+              "SELECT * FROM App.VHA_Stop_Code_Wait_Times_Paginated(1, 100)")) {
+        // statement.setString(1, icn);
+        try (ResultSet resultSet = statement.executeQuery()) {
+          ResultSetMetaData rsmd = resultSet.getMetaData();
+          int columnCount = rsmd.getColumnCount();
+          while (resultSet.next()) {
+            List<String> values = new ArrayList<>(columnCount);
+            for (int i = 1; i <= columnCount; i++) {
+              values.add(rsmd.getColumnName(i) + ":" + resultSet.getString(i));
+            }
+            results.add(values.stream().collect(Collectors.joining(" ")));
+          }
+        }
+      }
+      return "SELECT * FROM App.VHA_Stop_Code_Wait_Times_Paginated(1, 100)\n"
+          + results.stream().collect(Collectors.joining("\n"));
+    }
   }
 
   @RequestMapping(
