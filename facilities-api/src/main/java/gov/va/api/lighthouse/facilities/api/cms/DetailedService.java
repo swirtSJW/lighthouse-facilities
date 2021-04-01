bec76b1d0b6a53f6a09312f2af03fd7356370e41
bryan.schofield <vasdvp.jenkins@libertyits.com>
2021-04-01 15:52:23 +0000

    Release 2.0.97 - Jenkins Build 462 (department-of-veterans-affairs/lighthouse-facilities/master)

--- facilities-api/src/main/java/gov/va/api/lighthouse/facilities/api/cms/DetailedService.java
+++ facilities-api/src/main/java/gov/va/api/lighthouse/facilities/api/cms/DetailedService.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.Valid;
