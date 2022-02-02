922eb77c9e04dcf333076f6c6e9dbcf04da4c02e
Giang Nguyen <giang.nguyen@libertyits.com>
2022-02-02 15:41:57 -0500

    API-12978 add @Schema annotation to serviceId

--- facilities-api/src/main/java/gov/va/api/lighthouse/facilities/api/v1/DetailedService.java
+++ facilities-api/src/main/java/gov/va/api/lighthouse/facilities/api/v1/DetailedService.java
@@ -38,6 +38,7 @@ import org.apache.commons.lang3.ObjectUtils;
 @NoArgsConstructor
 @Schema(description = "Detailed information of a facility service.", nullable = true)
 public class DetailedService implements CanBeEmpty {
+  @Schema(description = "Service id.", example = "covid19Vaccine", nullable = true)
   String serviceId;
 
   @Schema(description = "Service name.", example = "COVID-19 vaccines", nullable = true)
